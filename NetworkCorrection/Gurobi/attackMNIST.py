9dd3ac62cb1c997940ea2a437e0618083de7d258
KhanTooba <jcs212245@iitd.ac.in>
2022-05-28 17:04:27 +0530

    Adversarial Image generation implemented and anomalous behaviour detected.

--- NetworkCorrection/Gurobi/attackMNIST.py
+++ NetworkCorrection/Gurobi/attackMNIST.py
@@ -59,21 +59,28 @@ def generateAattack():
     """
     1 stands for feasible model, 2 stands for infeasible model, 0 stands for error occurred while model creation.
     """
-    fileName = "MNISTdata/adversarialData.csv"
+    fileName = "MNISTdata/adversarialData-2.csv"
+    model = tf.keras.models.load_model(os.path.abspath(os.path.join(os.getcwd(), os.pardir)) +'/Models/mnist.h5')
     t1 = time()
     adversarial_data = []
+    anomaly = 0
     for i in range(count):
         inp = inputs[i]
         out = outputs[i]
         true_label = (np.argmax(out))
         num_outputs = len(out)
         num_inputs = len(inp)
-        model = tf.keras.models.load_model(os.path.abspath(os.path.join(os.getcwd(), os.pardir)) +'/Models/mnist.h5')
         # print(inp, "\nOutputs:", out, "\nTrueLabel:", true_label, "\nNum out:", num_outputs, "\nNum In:", num_inputs)
-        epsilons, status = find(1, 10, model, inp, true_label, num_inputs, num_outputs)
+        epsilons, status = find(0.2, 10, model, inp, true_label, num_inputs, num_outputs)
         status_codes[status] = status_codes[status] + 1
         if status==1:
             new_image = np.add(np.array(inp), np.array(epsilons))
+            predicted_output = model.predict([new_image.tolist()])
+            predicted_label = float(np.argmax(predicted_output))
+            if predicted_label==true_label:
+                # print("Found anomaly.", predicted_label, true_label)
+                anomaly = anomaly + 1
+                # break
             new_image = np.append(new_image,[true_label])
             adversarial_data.append(new_image)
         
@@ -82,7 +89,8 @@ def generateAattack():
             print("\n####################################")
             print("Adversarial images generated for: ",(i+1)," images.")
             print("Status till now: ", status_codes) 
-            print("Time taken till now: ",(t2 - t1), "seconds.")
+            print("Time taken till now: ",(t2 - t1)/3600, "hours.")
+            print("Anomalies found: ", anomaly)
             mode = 'a'
             if i==100:
                 mode = 'w'
---
+++ NetworkCorrection/Gurobi/verifyAttack.py
@@ -0,0 +1,42 @@
+from csv import reader
+import csv
+import os
+from re import I
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+"""
+To supress the tensorflow warnings. 
+0 = all messages are logged (default behavior)
+1 = INFO messages are not printed
+2 = INFO and WARNING messages are not printed
+3 = INFO, WARNING, and ERROR messages are not printed
+"""
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+"""
+Setting verbosity of tensorflow to minimum.
+"""
+import numpy as np
+
+
+f1 = open('MNISTdata/adversarialData-2.csv', 'r')
+f1_reader = reader(f1)
+count = 0 
+model = tf.keras.models.load_model(os.path.abspath(os.path.join(os.getcwd(), os.pardir)) +'/Models/mnist.h5')
+i = 0
+for row in f1_reader:
+    # count = count+1
+    if i%500==0 and i!=0:
+        print(count, i, (count/i*100),"%")
+    i = i + 1
+    length = len(row)
+    output = float(row[length-1])
+    inp = [float(x) for x in row[:length-1]]
+    predicted_output = model.predict([inp])
+    predicted_label = float(np.argmax(predicted_output))
+    # print(type(predicted_label), type(output))
+    if predicted_label != output:
+        count = count + 1
+    else:
+        print(inp, predicted_output, output)
+    # break
+print(count)
