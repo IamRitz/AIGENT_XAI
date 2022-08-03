88955bf335a85b66ed85f76ad89ca54245a71679
KhanTooba <jcs212245@iitd.ac.in>
2022-08-03 14:29:09 +0530

    updated broken files.

--- AttackMNIST/src/launchAttack.py
+++ AttackMNIST/src/launchAttack.py
@@ -1,7 +1,7 @@
 from time import time
-from attackMethod import attack
+from attackMethod import generate
 
 t1 = time()
-attack()
+generate()
 t2 = time()
 print("TIME TAKEN IN GENERATION OF ABOVE EXAMPLES: ", (t2-t1), "seconds.")
