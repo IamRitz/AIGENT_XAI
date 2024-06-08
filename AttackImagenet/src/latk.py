8ad82ee6dd51aaaf66370e7da64a5109f5fd92e3
IamRitz <26003726+IamRitz@users.noreply.github.com>
2024-06-08 23:06:22 +0530

    Cleaned

--- AttackImagenet/src/latk.py
+++
@@ -1,8 +0,0 @@
-from time import time
-# from attackMethod import attack
-from newatm import attack
-
-t1 = time()
-count  = attack()
-t2 = time()
-print("TIME TAKEN IN GENERATION OF ABOVE EXAMPLES: ", (t2-t1)/count, "seconds.")
